c8b76fed63ce8864be73fa446dce0a8542163cd7
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-17 15:28:49 -0300

    Add files via upload

---
+++ Exercicio2DOWHILE.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() 
+{
+	int numero = 0, numeroPar = 0;
+
+	setlocale(LC_ALL, "Portuguese");
+	system("color 17");
+
+	do 
+	{
+		numero++;
+	
+		while(numero % 2 == 0)
+		{
+			printf("Os numeros pares são:  %i\n", numero);
+			numero++;
+		}					
+
+	} while (numero >= 0 && numero <= 50);
+}
