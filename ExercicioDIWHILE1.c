7df04e18731b53dae8d257e65c4fc77ebc5fc69d
Rafael Felisbino Rocha da Silva <72631228+RafaelFelisbino-hub@users.noreply.github.com>
2020-10-17 15:47:45 -0300

    Update ExercicioDIWHILE1.c

--- ExercicioDIWHILE1.c
+++ ExercicioDIWHILE1.c
@@ -1,3 +1,9 @@
+/*Faça um algoritmo que leia dois números, o primeiro é o valor inicial de um
+contador, e o segundo é o valor final do contador (verifique se o valor inicial
+fornecido é inferior ao valor final). Usando a estrutura de repetição “repita”
+, escreva na tela uma contagem que comece no primeiro número lido,
+escreva os números seguintes colocando apenas um número em cada nova*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
